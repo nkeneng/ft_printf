9741ae80d39b61f7ded6c31658fe778088e351f5
stevennkeneng <nkeneng.junior@yahoo.fr>
2024-05-04 17:43:27 +0200

    removed unused variable

--- ft_printf.c
+++ ft_printf.c
@@ -15,18 +15,16 @@
 int	ft_printf(const char *str, ...)
 {
 	va_list	ptr;
-	int		incr;
 	int		counter;
 
 	counter = 0;
 	va_start(ptr, str);
-	incr = 0;
 	while (*str)
 	{
-		if (str[incr] == '%' && in_list(str[incr + 1]))
-			counter += ft_print_special(str++[incr + 1], &ptr);
+		if (str[0] == '%' && in_list(str[1]))
+			counter += ft_print_special(str++[1], &ptr);
 		else
-			counter += ft_printchar(str[incr]);
+			counter += ft_printchar(str[0]);
 		str++;
 	}
 	va_end(ptr);
